63d04b444cc7228e2d0e2f4686805a8c3b96727a
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2024-03-24 22:36:27 +0530

    Added styling

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     },
     {
       'id': 3,
-      'name': "Play a competitive sport",
+      'name': "Play a competitive sport and chill",
       'state': false
     }
 
@@ -38,17 +38,17 @@ function App() {
       <DndContext collisionDetection={closestCorners}>
           <div className='flex flex-col items-center'>
             {/* Form */}
-            <form className='flex flex-row text-md md:text-2xl mt-5 p-6 md:p-3 space-x-2'>
+            <form className='flex flex-row text-md md:text-lg lg:text-xl mt-5 p-6 md:p-3 space-x-2'>
               <input type='text' placeholder='Enter a task' className='p-1 border-2 border-slate-800 rounded-md'></input>
               <button type='submit' className='p-2 bg-green-600 text-white rounded-md'>Submit</button>
             </form>
 
             {/* Tasks */}
-            <div className='flex flex-col border-2 border-slate-500 rounded-md bg-gray-100 p-6 md:p-12 gap-y-4'>
+            <div className='flex flex-col border-2 border-slate-500 rounded-md bg-gray-100 p-6 md:p-12 gap-y-4 mt-5'>
               {tasks.map((task, index) => {
                   return(
-                    <div key={index} className='flex flex-between space-x-3 p-4 rounded-md border-2 border-slate-300'>
-                      <input type='checkbox'></input>
+                    <div key={index} className='flex flex-between items-center space-x-3 text-sm md:text-md lg:text-lg xl:text-xl m-1 md:m-2 lg:m-3 p-2 md:p-4 md:pr-24 rounded-md border-2 border-slate-300'>
+                      <input type='checkbox' className='h-4 w-4'></input>
                       <p>{task.name}</p>
                   </div>
                   )
