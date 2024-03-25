578baede11b0dbd8be2300e0ed7f37a1cff758a9
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2024-03-25 13:53:05 +0530

    Created task component

---
+++ src/data/data.js
@@ -0,0 +1,19 @@
+export const taskArray = [
+    {
+      'id': 1,
+      'name': "Wake up on time",
+      'state': false
+    },
+    {
+      'id': 2,
+      'name': "Eat healthy food",
+      'state': false
+    },
+    {
+      'id': 3,
+      'name': "Play a competitive sport and chill",
+      'state': false
+    }
+
+  ];
+
