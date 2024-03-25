6bbcdaad70fe182dcd61c8254e1237dc7945c8c6
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2024-03-25 14:17:31 +0530

    Functionality to add new task

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -7,13 +7,35 @@ const Tasks = () => {
     // Establish demo data
     const[tasks, setTasks] = useState(taskArray);
 
+    // Set new task
+    const[newTaskName, setNewTaskName] = useState("");
+
+    // Function: handle new task name
+    const handleNewTaskName = (e) => {
+        setNewTaskName(e.target.value);
+    }
+
+    // Function: handle creation of new tasks
+    const handleNewTaskCreation = () => {
+        let lastTaskIndex = tasks.length;
+
+        let taskObject = {
+          'id' : lastTaskIndex + 1,
+          'name' : newTaskName,
+          'updated' : false
+        }
+
+        setTasks([...tasks, taskObject]);
+    }
+
+
   return (
     <div className='flex flex-col items-center'>
             {/* Form */}
-            <form className='flex flex-row text-md md:text-lg lg:text-xl mt-5 p-6 md:p-3 space-x-2'>
-              <input type='text' placeholder='Enter a task' className='p-1 border-2 border-slate-800 rounded-md'></input>
-              <button type='submit' className='p-2 bg-green-600 text-white rounded-md'>Submit</button>
-            </form>
+            <div className='flex flex-row text-md md:text-lg lg:text-xl mt-5 p-6 md:p-3 space-x-2'>
+              <input type='text' name='taskName' value={newTaskName} placeholder='Enter a task' className='p-1 border-2 border-slate-800 rounded-md' onChange={handleNewTaskName}></input>
+              <button type='button' className='p-2 bg-green-600 text-white rounded-md' onClick={handleNewTaskCreation}>Submit</button>
+            </div>
 
             {/* Tasks */}
             <div className='flex flex-col border-2 border-slate-500 rounded-md bg-gray-100 p-6 md:p-12 gap-y-4 mt-5'>
